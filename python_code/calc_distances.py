a17f0f78461515085f724534ada569a36bbe0a6b
Garrett Allen <gm17alle@siena.edu>
2014-06-25 16:34:49 -0400

    Made a histogram for the random data entry via. CPU

--- python_code/calc_distances.py
+++ python_code/calc_distances.py
@@ -9,17 +9,14 @@ y = 10*np.random.random(npts)
 plt.figure()
 plt.plot(x,y,'o',markersize=1)
 plt.show()
-
+distance = [0]*500500
 count = 0
 for i in range(0, 1000):
-	if i%10==0:
-		print "halfway"
 	for j in range(i+1, 1000):
 		distance[count] =math.sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]))
 		count = count + 1
 
-	print "we are here"
 plt.figure()	
-plt.hist(x, bins=100)
+plt.hist(x, bins=20)
 plt.show()
 

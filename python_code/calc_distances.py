38d9869ef5fac83ef7ac29362bfc5621574ad9a0
Garrett Allen <gm17alle@siena.edu>
2014-06-25 16:05:04 -0400

    testing commit & some code

--- python_code/calc_distances.py
+++ python_code/calc_distances.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
-
+import math
 npts = 1000
 
 x = 10*np.random.random(npts)
@@ -9,3 +9,17 @@ y = 10*np.random.random(npts)
 plt.figure()
 plt.plot(x,y,'o',markersize=1)
 plt.show()
+
+count = 0
+for i in range(0, 1000):
+	if i%10==0:
+		print "halfway"
+	for j in range(i+1, 1000):
+		distance[count] =math.sqrt((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]))
+		count = count + 1
+
+	print "we are here"
+plt.figure()	
+plt.hist(x, bins=100)
+plt.show()
+
